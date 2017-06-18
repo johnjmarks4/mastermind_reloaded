dc1770746a90df71df7d1cd69c047ea0d2dbd793
johnjmarks4 <johnjmarks4@gmail.com>
2017-06-17 21:49:42 -0500

    Added display

--- mastermind_reloaded.rb
+++ mastermind_reloaded.rb
@@ -107,17 +107,23 @@ if player.class == Codemaker
     end
 
   get '/' do
+    @display = " "
+    @message = "Would you like to play as codebreaker or codemaker?"
     erb :index
   end
 
   get '/submit' do
+    #put display code here
+    @display = my_game.display
     role = params['role']
     if role == "codemaker"
       session["role"] = "Codemaker"
       @session = session
+      @message = 'Type your code below or type "rules" for a refresher on how to play.'
     elsif role == "codebreaker"
       session["role"] = "Codebreaker"
       @session = session
+      @message = 'Type your guesses below or type "rules" for a refresher on how to play.'
     elsif role == "rules" && session["role"] == "Codemaker"
       @message = File.read("codemaker_rules.txt")
     elsif role == "rules" && session["role"] == "Codebreaker"

3bc54d0138b3c0c9e5549e55504ffb07dbc50fa4
johnjmarks4 <johnjmarks4@gmail.com>
2017-06-19 12:15:25 -0500

    Display responds to player guess

--- mastermind_reloaded.rb
+++ mastermind_reloaded.rb
@@ -19,35 +19,65 @@ def computer_make_code
   @code
 end
 
+def compare_guess_to_code(guess, code)
+  i = 0
+  guess = guess.split(' ')
+  @display = session['display']
+  @display.each_with_index do |_, i|
+    if code[i] == guess[i]
+      @display[i] = "black"
+    elsif code.include?(guess[i])
+      @display[i] = "white"
+    else
+      @display[i] = "      "
+    end
+    i += 1
+    session['display'] = @display
+  end
+  @display
+end
+
 get '/' do
+  session['display'] = [" ", " ", " ", " "]
   session['turn'] = 0
   @turn = session['turn']
   @session = session
-  @display = " "
   @message = "Would you like to play as codebreaker or codemaker?"
   erb :index
 end
 
 get '/submit' do
-  @display = my_game.display
-  @role = params['role']
-  if @role == "codemaker"
-    session["role"] = "codemaker"
-    @message = 'Type your code below or type "rules" for a refresher on how to play.'
-  elsif @role == "codebreaker"
-    session["code"] = computer_make_code
-    session["role"] = "codebreaker"
-    @message = 'Type your guesses below or type "rules" for a refresher on how to play.'
-  elsif @role == "rules" && session["role"] == "Codemaker"
-    @message = File.read("codemaker_rules.txt")
-  elsif @role == "rules" && session["role"] == "Codebreaker"
-    @message = File.read("codebreaker_rules.txt")
-  elsif defined?(params['code'])
+  @display = session['display']
+  if defined?(params['role'])
+    @role = params['role']
+    session['role'] = params['role']
+  end
+  if params.has_key?('guess')
+    session['guess'] = params['guess']
+    @guess = params['guess']
+    session['turn'] == 0 ? @message = "Guess!" : @message = "Guess again!"
+  elsif params.has_key?('code')
     session['code'] = params['code']
     @code = params['code']
     @message = "The computer will now try to guess your code!"
+  elsif @role == "codemaker"
+    session["role"] = "codemaker"
+    @message = 'Type your code below or type "rules" for a refresher on how to play.'
+  elsif @role == "codebreaker"
+    session['code'] = computer_make_code
+    session['role'] = "codebreaker"
+    @message = 'Type your guesses below or type "rules" for a refresher on how to play.'
+  elsif @role == "rules" && session['role'] == "codemaker"
+    @message = File.read("codemaker_rules.txt")
+  elsif @role == "rules" && session['role'] == "codebreaker"
+    @message = File.read("codebreaker_rules.txt")
   else
     @message = 'Your input could not be understood. Please type either "codebreaker" or "codemaker".'
+  end
+
+  if session.has_key?('code') && session.has_key?('guess')
+    session['display'] = compare_guess_to_code(session['guess'], session['code'])
+    @display = session['display']
   end
 
   session['turn'] += 1
